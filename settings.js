fa10dcb144f98d4dfcfd269383c95394a9108eea
Gayachathu38 <107774729+Gayachathu38@users.noreply.github.com>
2022-06-19 03:28:59 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -19,9 +19,9 @@ global.APIKeys = {
 }
 
 //customize settings\\
-global.owner = ['Your Number']
-global.premium = ['Your Number']
-global.ownernomer = 'Your Number'
+global.owner = ['94741328832']
+global.premium = ['94741328832']
+global.ownernomer = '94741328832'
 global.ownername = '🐦Star Boy Nexus'
 global.botname = 'Gojo-Satoru'
 global.footer = '©NexusNwInc.'
